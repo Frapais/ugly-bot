ee4033f982bb1fb88adfc6619500e99f001efcf3
Konstantinos Paraskevas <65018944+Frapais@users.noreply.github.com>
2025-06-23 21:05:02 +0300

    Update main.cpp - corrected previous commit

--- microROS_bot/src/main.cpp
+++ microROS_bot/src/main.cpp
@@ -1,72 +1,93 @@
-#include "encoders.h"
-// #include <Arduino.h>
-#include <SPI.h>
-// #include "MT6701.h"
+#include <Arduino.h>
+#include <micro_ros_platformio.h>
 
-// Variables to store incremental values and previous angles
-float prev_angle_l = 0.0;
-float prev_angle_r = 0.0;
-long incremental_l = 0;
-long incremental_r = 0;
+#include <rcl/rcl.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
 
-// Encoder objects
-MT6701 encoder_l;
-MT6701 encoder_r;
+#include <std_msgs/msg/int32.h>
 
-void encodersSetup() {
-    // Serial.begin(115200);
-    SPI.begin();
-    encoder_l.initializeSSI(CS_PIN_L);
-    encoder_r.initializeSSI(CS_PIN_R);
-    Serial.println("Encoders initialized");
+#include <encoders.h>
+
+
+// #if !defined(MICRO_ROS_TRANSPORT_ARDUINO_SERIAL)
+// #error This example is only avaliable for Arduino framework with serial transport.
+// #endif
+
+rcl_publisher_t publisher;
+std_msgs__msg__Int32 msg;
+
+rclc_executor_t executor;
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_node_t node;
+rcl_timer_t timer;
+
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+
+// Error handle loop
+void error_loop() {
+  while(1) {
+    delay(100);
+  }
 }
 
-long updateEncoderL() {
-    // Read angles
-    float angle_l = encoder_l.angleRead();
-
-    // Calculate incremental values for left encoder
-    float delta_l = angle_l - prev_angle_l;
-    if (delta_l > 180.0) {
-        delta_l -= 360.0; // Handle wrap-around from 360 to 0
-    } else if (delta_l < -180.0) {
-        delta_l += 360.0; // Handle wrap-around from 0 to 360
-    }
-    incremental_l += (long)(delta_l);
-
-   
-    // Update previous angles
-    prev_angle_l = angle_l;
-
-    // Print incremental values
-    // Serial.print("Incremental L: ");
-    // Serial.print(incremental_l);
-    
-    // delay(100);
-    return(incremental_l);
+void timer_callback(rcl_timer_t * timer, int64_t last_call_time) {
+  RCLC_UNUSED(last_call_time);
+  if (timer != NULL) {
+    RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
+    msg.data=(int32_t)updateEncoderL();
+  }
 }
 
-long updateEncoderR() {
-    // Read angles
-    float angle_r = encoder_r.angleRead();
+void setup() {
+  IPAddress agent_ip(192, 168, 68, 64);
+  size_t agent_port = 8888;
 
+  char ssid[] = "Spiti Network";
+  char psk[]= "SP191215";
 
-    // Calculate incremental values for right encoder (reversed direction)
-    float delta_r = angle_r - prev_angle_r;
-    if (delta_r < -180.0) {
-        delta_r += 360.0; // Handle wrap-around from 360 to 0
-    } else if (delta_r > 180.0) {
-        delta_r -= 360.0; // Handle wrap-around from 0 to 360
-    }
-    incremental_r -= (long)(delta_r); // Reverse the direction by negating the delta
+  // Initialize encoders
+  encodersSetup();
 
-    // Update previous angles
-    prev_angle_r = angle_r;
+  set_microros_wifi_transports(ssid, psk, agent_ip, agent_port);
+  // Configure serial transport
+  // Serial.begin(115200);
+  // set_microros_serial_transports(Serial);
+  delay(2000);
 
-    // Print incremental values
-    // Serial.print("   |   Incremental R: ");
-    // Serial.println(incremental_r);
-    
-    // delay(100);
-    return(incremental_r);
+  allocator = rcl_get_default_allocator();
+
+  //create init_options
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+  // create node
+  RCCHECK(rclc_node_init_default(&node, "micro_ros_platformio_node", "", &support));
+
+  // create publisher
+  RCCHECK(rclc_publisher_init_default(
+    &publisher,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+    "micro_ros_platformio_node_publisher"));
+
+  // create timer,
+  const unsigned int timer_timeout = 500;
+  RCCHECK(rclc_timer_init_default(
+    &timer,
+    &support,
+    RCL_MS_TO_NS(timer_timeout),
+    timer_callback));
+
+  // create executor
+  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+  RCCHECK(rclc_executor_add_timer(&executor, &timer));
+
+  msg.data = 0;
+}
+
+void loop() {
+  delay(100);
+  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
 }
